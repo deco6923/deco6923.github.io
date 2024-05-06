3f3e39b651a708f346d335cfa2c246487703e856
Destiny Cool <deco6923@colorado.edu>
2024-05-05 20:54:27 -0600

    new

--- Java-Final/java-final.js
+++ Java-Final/java-final.js
@@ -1,8 +1,5 @@
 const userInput = prompt("Are you ready to waste at least 1 minute of your time? y/n")
 
-const myDiv = document.querySelector('div');
-const button1 = document.getElementById('button1')
-
 if (userInput === "y" || userInput === "yes"){
     alert("Welp... Good Luck")
 } else if (userInput === "n" || userInput === "no") {
@@ -11,11 +8,61 @@ if (userInput === "y" || userInput === "yes"){
     alert("Doesn't matter... Let's continue")
 }
 
+let volume = document.getElementById('volume')
+let newVolume = 0;
 let colorIndex = 0
-function changeColor(event){
-    const colors = ['grey', 'pink', 'blue', 'yellow', 'purple', 'orange', 'red'];
+function changeColor1(event){
+    const colors = ['grey', 'pink', 'red', 'rgb(186, 44, 115)', 'green', 'orange', 'red', 'purple'];
     event.style.backgroundColor = colors[colorIndex];
-    colorIndex = (colorIndex + 1) % colors.length;
+    colorIndex = (colorIndex + 1) % colors.length; 
 
-    // if the color of the button = red then add 5 to the volume
+    if (colorIndex == 3 || colorIndex == 7){
+        newVolume += 5;
+        volume.innerHTML = `Current Volume: ${newVolume}`
+    } 
+
+}
+
+function changeColor2(event){
+    const colors = ['grey', 'pink', 'red', 'rgb(186, 44, 115)', 'green', 'orange', 'red', 'purple'];
+    event.style.backgroundColor = colors[colorIndex];
+    colorIndex = (colorIndex + 1) % colors.length; 
+
+    if (colorIndex == 3 || colorIndex == 7){
+        newVolume -= 5;
+        volume.innerHTML = `Current Volume: ${newVolume}`
+    } 
+}
+
+function changeColor3(event){
+    const colors = ['grey', 'darkpink', 'red', 'rgb(186, 44, 115)', 'green', 'orange', 'red', 'purple'];
+    event.style.backgroundColor = colors[colorIndex];
+    colorIndex = (colorIndex + 1) % colors.length; 
+
+    if (colorIndex == 3 || colorIndex == 7){
+        newVolume += 1;
+        volume.innerHTML = `Current Volume: ${newVolume}`
+    } 
+}
+
+function changeColor4(event){
+    const colors = ['grey', 'pink', 'red', 'rgb(186, 44, 115)', 'green', 'orange', 'red', 'purple'];
+    event.style.backgroundColor = colors[colorIndex];
+    colorIndex = (colorIndex + 1) % colors.length; 
+
+    if (colorIndex == 3 || colorIndex == 7){
+        newVolume -= 1;
+        volume.innerHTML = `Current Volume: ${newVolume}`
+    } 
+}
+
+let resetBtn = document.getElementById('reset');
+let new1 = 0
+function resetVolume(){
+    volume.innerHTML = `Current Volume: ${new1}`
+    newVolume = 0
+    button1.style.backgroundColor = 'palevioletred'
+    button2.style.backgroundColor = 'aqua'
+    button3.style.backgroundColor = 'yellow'
+    button4.style.backgroundColor = 'greenyellow'
 }
